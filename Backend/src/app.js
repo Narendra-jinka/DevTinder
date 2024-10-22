808df6a5c6ce2b46aaaa5a9c796ed8986c193f12
Narendra Jinka <narendrajinka44@gmail.com>
2024-10-22 12:58:05 +0530

    http methodes

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,17 +1,27 @@
 const express = require('express') ; // importing from node_modules
 const app =express();// using express instance to create server .. 
+
+
+app.get("/user",(req,res)=>{
+    res.send({
+        firstName:"Narendra",
+        lastName : " jinka"
+    });
+});
+app.post("/user",(req,res)=>{
+    res.send("Data Posted successfully ");
+});
+
+app.delete("/user" , (req,res)=>{
+    res.send("Data deleted successfully ")
+})
+
 app.use("/test",(req,res)=>{
     res.send("hello this is server/test responding..");
     
 });
-app.use("/demo",(req,res)=>{
-    res.send("hello this is server/demo123 responding..");
-    
-});
-app.use("/",(req,res)=>{
-    res.send("hello this is server/ responding..");
-    
-});
+
+
 app.listen(3344,()=>{
     console.log("app listening at 3000 port ..") 
 });
