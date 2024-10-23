601abae87c11b2fba4e22d54eecc7abd6dfee9bb
Narendra Jinka <narendrajinka44@gmail.com>
2024-10-24 01:09:14 +0530

    Exploring the Validator Lib

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -12,16 +12,16 @@ app.post("/signup", async (req, res) => {
     await user.save();
     res.send("Data added successfully!!");
   } catch (err) {
-    res.status(400).send("error is occured :"+ err.message);
+    res.status(400).send("error is occured :" + err.message);
   }
 });
 
 // get a single user .  are list of user who are matched .
 app.get("/users", async (req, res) => {
-    const userEmail = req.body.email;
-    console.log(userEmail);
+  const userEmail = req.body.email;
+  console.log(userEmail);
   try {
-    const users = await User.find({ email:  userEmail});
+    const users = await User.find({ email: userEmail });
     if (users.length === 0) {
       res.status(404).send("user not found");
     } else {
@@ -32,40 +32,64 @@ app.get("/users", async (req, res) => {
   }
 });
 
-//get all users 
-app.get("/feed", async (req,res)=>{
-    try{
-        const users = await User.find({});
-        res.send(users);
-    } catch (err) {
-        res.status(400).send("some thing went wrong");
-      }
-})
+//get all users
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("some thing went wrong");
+  }
+});
 
-app.delete("/user",async (req,res)=>{
-    const mail =req.body.email ; 
+app.delete("/user", async (req, res) => {
+  const mail = req.body.email;
 
-    try{
-        const user = await User.findOneAndDelete({email:mail});
-        res.send("user deleted successfully");
+  try {
+    const user = await User.findOneAndDelete({ email: mail });
+    res.send("user deleted successfully");
+  } catch (err) {
+    res.status(400).send("some thing went wrong");
+  }
+});
 
-    }catch (err) {
-        res.status(400).send("some thing went wrong");
-      }
-})
-
-app.patch("/user", async (req,res)=>{
-    const mail = req.body.email;
-    const body = req.body;
-    try{
-        const user = await User.findOneAndUpdate({email:mail},body,{
-          runValidators: true
-        });
-        res.send("user updated successfully ");
+app.patch("/user", async (req, res) => {
+  const mail = req.body.email;
+  const body = req.body;
+  try {
+    const user = await User.findOneAndUpdate({ email: mail }, body, {
+      runValidators: true,
+    });
+    res.send("user updated successfully ");
+  } catch (err) {
+    res.status(400).send("some thing went wrong " + err.message);
+  }
+});
+app.patch("/user/:email", async (req, res) => {
+  const mail = req.params?.email;
+  const body = req.body;
+  try {
+    const ALLOW_UPDATE = [
+      "password",
+      "firstName",
+      "lastName",
+      "age",
+      "gender",
+      "photoId",
+      "description",
+      "skills",
+    ];
+    const isAllowed = Object.keys(body).every((k) => ALLOW_UPDATE.includes(k));
+    if (!isAllowed) {
+      throw new Error("Update Not allowed");
     }
-    catch (err) {
-        res.status(400).send("some thing went wrong "+err.message);
-      }
+    const user = await User.findOneAndUpdate({ email: mail }, body, {
+      runValidators: true,
+    });
+    res.send("user updated successfully ");
+  } catch (err) {
+    res.status(400).send("Update Failed:  " + err.message);
+  }
 });
 
 connectdb()
--- Backend/src/models/user.js
+++ Backend/src/models/user.js
@@ -1,56 +1,78 @@
 const mongoose = require("mongoose");
-
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required : true,
-    minLength: 3,
-    maxLength:50,
+const validator = require("validator");
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 3,
+      maxLength: 50,
+    },
+    lastName: {
+      type: String,
+      minLength: 3,
+      maxLength: 50,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("InValid Email Id");
+        }
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      validate(value) {
+        if (!validator.isStrongPassword(value)) {
+          throw new Error(
+            " the password must contains min 8 chars and atleast 1Uppercase, 1Lowercase, 1Number, 1Special Char"
+          );
+        }
+      },
+    },
+    age: {
+      type: Number,
+      min: 18,
+    },
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error("Gender is Invalid ..");
+        }
+      },
+    },
+    photoId: {
+      type: String,
+      default: "",
+      validate(value) {
+        if (validator.isURL(value)) {
+          throw new Error("inValid Photo URL");
+        }
+      },
+    },
+    description: {
+      type: String,
+      default: "write your own description .. !! ",
+    },
+    skills: {
+      type: [String],
+      validate(value) {
+        if (value.length > 10) {
+          throw new Error("The Skills Must be less Then 10");
+        }
+      },
+    },
   },
-  lastName: {
-    type: String,
-    minLength: 3,
-    maxLength:50,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique : true,
-    lowercase : true,
-    trim : true,
-    
-  },
-  password: {
-    type: String,
-    required:true,
-    minLength :8,
-     
-  },
-  age: {
-    type: Number,
-    min : 18,
-  },
-  gender: {
-    type: String,
-    validate(value){
-      if(!["male","female","others"].includes(value)){
-        throw new Error("Gender is Invalid ..");
-      }
-    }
-  },
-  photoId:{
-    type: String,
-    default : ""
-  },
-  description :{
-    type : String,
-    default : "write your own description .. !! "
-  },
-  skills :{
-    type : [String],
-  },
-},{
-  timestamps : true
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("User", userSchema);
