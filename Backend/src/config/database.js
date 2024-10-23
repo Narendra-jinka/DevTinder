959037a30d59ad400b248dfa06b854ebc8fd04b7
Narendra Jinka <narendrajinka44@gmail.com>
2024-10-23 23:01:08 +0530

    random commit

--- Backend/src/config/database.js
+++ Backend/src/config/database.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+//mongodb+srv://DevTinderDB:backendDB@devtinderapp.8m1hw.mongodb.net/
 const contectdb = async () => {
   await mongoose.connect(
     "mongodb+srv://DevTinderDB:backendDB@devtinderapp.8m1hw.mongodb.net/devTinder"
