5d0af554796eff0a03b5c1b031d9937878f40ddd
Narendra Jinka <narendrajinka44@gmail.com>
2025-01-20 11:05:12 +0530

    Final Backend Commit

--- Backend/src/Routes/user.js
+++ Backend/src/Routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const userRoute = express.Router();
 const { userAuth } = require("../Middlewares/Auth");
 const ConnectionRequest = require("../models/ConnectionRequest");
+const User = require("../models/User");
 const USER_ALLOWED_FIELDS =
   "firstName lastName age about skills photoId gender";
 
@@ -46,6 +47,45 @@ userRoute.get("/user/connections", userAuth, async (req, res) => {
       data,
     });
   } catch (error) {}
+});
+
+userRoute.get("/feed",userAuth,async(req,res)=>{
+  try {
+    const loggedInUser = req.user;
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10 ;
+    limit  = limit > 50 ? 50 : limit;
+    const skip = (page-1)*limit;
+
+    const connectionRequests = await ConnectionRequest.find({
+      $or : [
+        {fromUserId :loggedInUser._id },{toUserId : loggedInUser._id}
+      ]
+    }).select("fromUserId toUserId");
+
+    const hideProfiles = new Set();
+    connectionRequests.forEach((conReq)=>{
+      hideProfiles.add(conReq.fromUserId.toString());
+      hideProfiles.add(conReq.toUserId.toString());
+    });
+
+    const data = await User.find({
+      $and :[
+        {_id : {$nin : Array.from(hideProfiles)}},
+        {_id : {$ne : loggedInUser._id}}
+      ]
+    }).select(USER_ALLOWED_FIELDS).skip(skip).limit(limit);
+
+
+    res.json({
+      message:"Feed Fetched Successfully",
+      data,
+    })
+  } catch (error) {
+    res.status(400).send({
+      message:error.message
+    })
+  }
 });
 
 module.exports = userRoute;
